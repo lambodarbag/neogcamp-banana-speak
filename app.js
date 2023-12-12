352d403fab579a3acd3fd601e16e98bb05d59f78
Lambodar Bag <152137585+lambodarbag@users.noreply.github.com>
2023-12-13 04:55:59 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
 
+console.log(txtInput);
+
 function clickHandler(){
     console.log("clicked");
     console.log("input", txtInput.value);

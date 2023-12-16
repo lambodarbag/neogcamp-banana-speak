dc1a257129b7b68d51fafd9ea58ffd5900c4320c
Lambodar Bag <152137585+lambodarbag@users.noreply.github.com>
2023-12-16 14:54:29 +0530

    dom

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
 let outputDiv = document.querySelector("#output");
 
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+   
+}
+
+function errorHandler(error){
+    console.log("error occure", error);
+    alert("something wrong with server, please try after sometime")
+}
+
 
 function clickHandler(){
-     outputDiv.innerText =txtInput.value;
+    let inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        let translatedText = json.contents.translated;
+         outputDiv.innerText = translatedText;
+        })
+    .catch(errorHandler)
+
 };
 
 btnTranslate.addEventListener("click", clickHandler);
